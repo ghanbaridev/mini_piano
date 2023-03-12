ce51483fe8dcf181a9f57fa50bea081f358eb369
ghanbaridev <104554904+ghanbaridev@users.noreply.github.com>
2023-03-12 16:48:33 +0330

    remoed denug flag

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class piano extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
